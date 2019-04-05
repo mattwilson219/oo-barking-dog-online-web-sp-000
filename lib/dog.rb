2499a2e8dec7708e6a75c27ba331d850f5a84ee0
Matthew Wilson <wilsonmatt219@gmail.com>
2019-04-05 19:29:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,12 @@
 # Your code goes here!
 class Dog
-  def name=(name)
-    name = name
+  def name= (name)
+    @name = name
   end
-
   def name
     @name
   end
-end
-
-class Dog
   def bark
-    puts "Woof!"
+    puts "woof!"
   end
 end
